fc6aa7261198da7c4e532d2b65392f386c02e2ec
pleahmacaka <pleahmacaka@gmail.com>
2025-05-19 11:27:57 +0900

    feat: add example project

---
+++ example/src/lib/components/CustomToast.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+import { Bread, type BreadProps } from "svelte-daisy-toaster"
+
+let { text = "Empty!", ...props }: BreadProps & { text: string } =
+  $props()
+</script>
+
+<Bread {...props}>
+  <p class="text-white">{text}</p>
+</Bread>
---
+++ example/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+import CustomToast from "$lib/components/CustomToast.svelte"
+import type { ComponentProps } from "svelte"
+import { createToster, Toster } from "svelte-daisy-toaster"
+
+let toster = createToster<ComponentProps<typeof CustomToast>>()
+toster.component = CustomToast
+</script>
+
+<Toster />
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<button class="btn btn-info alert" onclick={() => toster.info({ text: "blabla" })}>
+  Info
+</button>
