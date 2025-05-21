cba2e843da37ed672dc8773861373d16b8fd3fe9
pleahmacaka <pleahmacaka@gmail.com>
2025-05-21 10:41:13 +0900

    feat: update example

--- example/src/lib/components/CustomToast.svelte
+++ example/src/lib/components/CustomToast.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
 import { Bread, type BreadProps } from "svelte-daisy-toaster"
 
-let { text = "Empty!", ...props }: BreadProps & { text: string } =
-  $props()
+let {
+  text = "Empty!",
+  ...props
+}: BreadProps & {
+  text: string
+} = $props()
 </script>
 
 <Bread {...props}>
-  <p class="text-white">{text}</p>
+  <p class="text-white">
+    🦆 {text}
+  </p>
 </Bread>
